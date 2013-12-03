16b608d29ab00fd1fa1c335d49df8c336e2f20fc
mgechev <mgechev@gmail.com>
2013-12-03 10:38:01 +0200

    Removed some !important stuff

--- resources/tutorials/isolated-scope/bind-method/bootstrap.js
+++ resources/tutorials/isolated-scope/bind-method/bootstrap.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MainCtrl', function ($scope, $timeout) {
+myApp.controller('MainCtrl', function ($scope) {
   $scope.alertMe = function () {
     alert('Foo');
   };
